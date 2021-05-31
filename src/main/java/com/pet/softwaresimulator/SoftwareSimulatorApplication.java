0ff25b15991ff304cd55bbee2372deb649cd19e0
overpathz <overpathz@gmail.com>
2021-05-31 20:17:33 +0300

    add project

---
+++ src/main/java/com/pet/softwaresimulator/SoftwareSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.pet.softwaresimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pet/softwaresimulator/SoftwareSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.softwaresimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
